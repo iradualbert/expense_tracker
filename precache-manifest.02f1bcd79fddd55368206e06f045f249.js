2a4c4dddf51993ad135cf5e88b6e778c52501b43
Albert Iradukunda <albert.iradukunda@yahoo.com>
2020-10-13 21:44:22 +0300

    Updates

---
+++ precache-manifest.02f1bcd79fddd55368206e06f045f249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce9d2419e29523c02bd641aac5b5158",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "fa5f5c5af46e6d5dd37f",
+    "url": "/expense-tracker/static/css/main.2e8f2e91.chunk.css"
+  },
+  {
+    "revision": "d274357ae37698f69cd8",
+    "url": "/expense-tracker/static/js/2.87902a96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.87902a96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa5f5c5af46e6d5dd37f",
+    "url": "/expense-tracker/static/js/main.5b1ac85a.chunk.js"
+  },
+  {
+    "revision": "2a39391c79534067f41a",
+    "url": "/expense-tracker/static/js/runtime-main.84524912.js"
+  }
+]);
